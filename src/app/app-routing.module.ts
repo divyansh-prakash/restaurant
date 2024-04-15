358c76e805e8783b2ac5a11d3a32d9f0366ebd55
divyansh.sharma <divyansh.sharma@daffodilsw.com>
2024-04-15 14:36:15 +0530

    removing home route for now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
   // { path: '**', redirectTo: 'home' }
 ];
 
